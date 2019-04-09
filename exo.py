ad65b6f555e69201a50d4d51fda83e89707dce3b
sidibemohamadou <mouhamadousidibe97@gmail.com>
2019-04-09 10:44:05 +0000

    first commit

---
+++ exo.py
@@ -0,0 +1,18 @@
+#creation de la variable 'x'
+
+x=int(input("donner la valeur de  x"))
+print("la valeur de",x)
+
+#creation de la variable 'y'
+
+y=int(input("donner la valeur de y"))
+print("la valeur de ",y)
+
+# la somme des deux valeut 
+
+somme = x + y 
+print("la somme est egale a",somme)
+
+# la moyenne des deux valeurs
+moyenne = somme/2
+print("la moyenne est de",moyenne)
