54c12d356a7635adb25e7395b934070a20609705
sidibemohamadou <49401334+sidibemohamadou@users.noreply.github.com>
2019-04-10 10:12:38 +0000

    programme portant  sur les nombres pair,impair

---
+++ nombre_paire.py
@@ -0,0 +1,17 @@
+
+#creation de variable
+
+nb = int (input("veuiller donner la valeur de nb"))
+
+print (nb)
+if nb % 2 == 0 :
+
+   print("le nombre est pair")
+
+elif nb % 2 != 0 :
+
+  print("le nombre est impair")
+
+else : 
+ print("donner une autre valeur ")   
+
