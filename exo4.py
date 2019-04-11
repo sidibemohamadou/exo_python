bab050110b2991b952eb05778bd3f18249e1a83d
sidibemohamadou <49401334+sidibemohamadou@users.noreply.github.com>
2019-04-11 12:55:48 +0000

    exercice avec le boucle for les fonctions random,sys

---
+++ exo4.py
@@ -0,0 +1,16 @@
+import random
+import sys
+nombre_secret = random.randint(0,9)
+answer =None
+print(nombre_secret)
+for answer in range(3)
+      answer = int(input("entrer une valeur comprise entre 0 et 9")
+     if  answer > nombre_secret :
+       print("le nombre_secret est inferieur au valeur donner")
+     elif answer  < nombre_secret :
+       print("le nombre_secret est superieur au valeur donner")
+     else:
+       print("bravo,vous avez gagné")
+        sys.exit()
+print("malheureusement vous avez perdu")
+
