8a084f428d173623b02e7894f2cf938d84cf02d3
sidibemohamadou <49401334+sidibemohamadou@users.noreply.github.com>
2019-04-11 12:54:41 +0000

    correction avec le boucle while true:

--- alea.py
+++ alea.py
@@ -1,15 +1,14 @@
-#variable aleatoire
+#exercice portant sur le nombre de repetition
 import random
 nombre_secret = random.randint(0,9)
-answer = none
-do{ answer = int(input("entrer une valeur comprise entre 0 et 9")
-                 }
-while answer != nombre_secret :
-     
-     if  answer > nombre_secret :
-       print("le nombre_secret est inferieur au valeur donner")
-     elif answer < nombre_secret :
-       print("le nombre_secret est superieur au valeur donner")
-     else:
+retour = none
+retour = int(input("entrer une valeur comprise entre 0 et 9"
+while True:
+     if retour == nombre_secret:
        print("bravo,vous avez gagné")
+     elif  retour > nombre_secret:
+       print("le nombre_secret est inferieur au valeur donner")
+     else:
+       print("le nombre_secret est superieur au valeur donner")
+     break
  
