bc720caca3e130bc7400ecc06d015908f9d8a287
sidibemohamadou <49401334+sidibemohamadou@users.noreply.github.com>
2019-04-11 11:57:09 +0000

    correction de l'exercice 03 python avec do while

--- alea.py
+++ alea.py
@@ -2,11 +2,13 @@
 import random
 nombre_secret = random.randint(0,9)
 answer = none
+do{ answer = int(input("entrer une valeur comprise entre 0 et 9")
+                 }
 while answer != nombre_secret :
-      answer = int(input("entrer une valeur comprise entre 0 et 9")
+     
      if  answer > nombre_secret :
        print("le nombre_secret est inferieur au valeur donner")
-     elif answer  nombre_secret :
+     elif answer < nombre_secret :
        print("le nombre_secret est superieur au valeur donner")
      else:
        print("bravo,vous avez gagné")
